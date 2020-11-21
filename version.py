255dc1b32280ab055cc712899943bed8d58adb1f
Mikhail Zakharov <mzakharo@gmail.com>
2020-11-21 08:53:54 -0500

    add print

--- version.py
+++ version.py
@@ -1,4 +1,5 @@
 import subprocess
 version = subprocess.check_output('git describe --tag --dirty', shell=True).decode('utf-8').strip()
+print('Version:', version)
 with open('RELEASE_VERSION.py', 'w') as f:
     f.write(f'__version__ = "{version}"\n')
