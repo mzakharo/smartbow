35fa6daa820b39d16c750e63a48bffda38a923a7
Mikhail Zakharov <mikhail.zakharov@cognitivesystems.com>
2021-02-15 14:11:47 -0500

    setenv

--- version.py
+++ version.py
@@ -1,5 +1,7 @@
 import subprocess
-version = subprocess.check_output('git describe --tag --dirty', shell=True).decode('utf-8').strip()
+import os
+
+version = os.getenv('RELEASE_VERSION', None) or subprocess.check_output('git describe --tag --dirty', shell=True).decode('utf-8').strip()
 print('Version:', version)
 with open('RELEASE_VERSION.py', 'w') as f:
     f.write(f'__version__ = "{version}"\n')
